75a8fd0a9d6025b216c92d8945551e744403c5f0
Kinnard <khockenhull@gmail.com>
2015-03-31 20:14:12 -0700

    Added shell script and routes and fixed social links

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,32 @@
 var express = require('express');
 var router = express.Router();
 
+var shell = require('shelljs')
+
 /* GET home page. */
 router.get('/', function(req, res) {
-  res.render('index', { title: 'Express' });
+  res.render('index', { title: 'Black Bitcoiners' });
 });
+
+
+function setEnvVars (req, cb){
+    shell.env['email']  = req.body.email;
+    shell.env['source'] = req.body.source;
+    cb();
+};
+
+router.post('/submitemail', function(req,res){
+    console.log(req.body.email);
+    console.log(req.body.source);
+    setEnvVars(req, function(){
+        shell.exec('./bash/email.sh',function(err){
+            if (err) console.log(err);
+        });
+
+        res.render('awesome');
+    });
+    
+});
+
 
 module.exports = router;
